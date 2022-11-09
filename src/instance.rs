abaa1132ee2d69a9c5fb438c5bc8deb0e7082451
Lucas Newcomb <pantsbabyy@gmail.com>
2022-11-09 12:14:03 -0500

    Removed model.ipynb

--- src/instance.rs
+++ src/instance.rs
@@ -7,7 +7,7 @@ pub struct Instance {
     pub y: i32,
     pub width: u32,
     pub height: u32,
-    // pub number: u32,
+    pub number: u32,
 }
 
 impl Default for Instance {
@@ -21,7 +21,7 @@ impl Default for Instance {
             y: 0,
             width: display_width / display_cols,
             height: display_height / display_rows,
-            // number: 0,
+            number: 0,
         }
     }
 }
@@ -30,17 +30,11 @@ impl Instance {
     pub fn screenshot(&self, file_name: String) -> Result<(), std::io::Error> {
         let screens = Screen::all().unwrap();
         let primary_screen = screens[0];
-
         let image = primary_screen.capture_area(self.x, self.y, self.width, self.height).unwrap();
         let buffer = image.buffer();
         fs::create_dir_all("screenshots")?;
         fs::write(format!("screenshots/{}.png", file_name), buffer).unwrap();
-        println!("Screenshotted (x: {}, y: {}, width: {}, height: {}", self.x, self.y, self.width, self.height);
+        println!("Screenshotted Instance {} (x: {}, y: {}, width: {}, height: {}", self.number, self.x, self.y, self.width, self.height);
         Ok(())
     }
-
-    // pub fn get_instance_number(&self) -> u32 {
-    //     (self.x as u32 / self.width * Display::default().cols) + (self.y as u32 / self.height) + 1_u32
-    // }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate tensorflow;
+
 mod display;
 mod instance;
 
@@ -7,12 +9,14 @@ use instance::Instance;
 
 fn main() {
     let display = Display::default();
+
     for row in 1..display.rows + 1 {
-        for i in 1..display.cols + 1 {
+        for col in 1..display.cols + 1 {
             let mut inst = Instance::default();
-            inst.x = ((row - 1) * inst.width) as i32;
-            inst.y = ((i - 1) * inst.height) as i32;
-            inst.screenshot(format!("Screenshot(row: {}, col: {}", row, i))
+            inst.x = ((col - 1) * inst.width) as i32;
+            inst.y = ((row - 1) * inst.height) as i32;
+            inst.number = col + ((row - 1) * 3);
+            inst.screenshot(format!("Instance {}", inst.number))
                 .map_err(|err| println!("{:?}", err)).ok();
         }
     }
