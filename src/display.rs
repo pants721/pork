2e23c73ae971e6d675523407bdea3acdc7d3e17d
Lucas Newcomb <pantsbabyy@gmail.com>
2022-11-21 15:32:19 -0500

    Declared instance object instead of default

--- src/display.rs
+++ src/display.rs
@@ -22,16 +22,20 @@ impl Display {
         let start = Instant::now();
         for row in 1..=self.rows {
             for col in 1..=self.cols {
-                let mut inst = Instance::default();
-                inst.x = ((col - 1) * inst.width) as i32;
-                inst.y = ((row - 1) * inst.height) as i32;
-                inst.number = col + ((row - 1) * self.cols);
+                let mut inst = Instance {
+                    x: ((col - 1) * self.width / self.cols) as i32,
+                    y: ((row - 1) * self.height / self.rows) as i32,
+                    width: self.width / self.cols,
+                    height: self.height / self.rows,
+                    number: col + ((row - 1) * self.cols),
+                    sc_dir: String::new(),
+                };
                 let inst_thread = thread::spawn(move || inst.run());
                 thread_vec.push(inst_thread);
             }
         }
 
-        // Joins all threads in array
+        // Joins all threads in vec 
         for thread in thread_vec {
             thread.join().unwrap();
         }
